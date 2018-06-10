7222ffd91372c60d1ee96d0b7fa4e2ff74abf7b8
ki <ishukalraish2@gmail.com>
2018-06-10 06:38:34 +0530

    First elementary program :p

---
+++ WhatIsC++?/Mileage.cpp
@@ -0,0 +1,21 @@
+#include <iostream>  //Reading and writing to files
+#include <iomanip>
+using namespace std;   //std allows standard function accessibility
+int main()            // Must be there in every program
+{
+    //Each line executes sequentially
+
+    double mpg, distance, gallons, pricePerGallon, totalCost;
+    //output using insertion operator
+    cout<<"Enter mpg"<<endl; //Send message to User
+    //input using extraction operator
+    cin>>mpg;  //cin reads from console, cout writes to console.
+
+    gallons = distance/mpg;
+
+    totalCost = gallons * pricePerGallon;
+    //Set precision makes it upto 2 decimal points
+    cout<<"Total trip cost: $"<<fixed<<setprecision(2)<<totalCost<<endl;
+
+    return 0;
+}
