0cbe03f03a009f2e47a91676ba75353d580e3785
ki <ishukalraish2@gmail.com>
2018-06-27 10:59:18 +0530

    Commonly used functions and their respective libraries

---
+++ predefinedFunctions.cpp
@@ -0,0 +1,57 @@
+//<string>
+/*
+string getline()
+int length()
+string substr(int pos)
+string substr(int start, int len)
+*/
+
+//<cmath>
+/*
+sqrt
+pow
+sin
+cos
+fabs
+*/
+
+//<iomanip>
+/*
+fixed(trailing zeros)
+left, right
+setfill(char fillchar)
+setprecision(int num)
+setw(int width)
+*/
+
+//<cstdlib>
+/*
+int rand()
+void srand(int num)
+int abs(int num)
+void exit(int num)
+*/
+
+//<cctype>
+/*
+bool isaplha(char c)
+bool isdigit(char c)
+bool islower(char c)
+bool isupper(char c)
+bool isspace(char c)
+*/
+
+//RANDOM NUMBERS
+/*
+Pseudorandom numbers
+Random numbers with bounds a and b
+Generic form: (rand() * (b - a + 1)) + a
+Die: rand()*6 + 1;
+*/
+
+
+
+
+
+
+
